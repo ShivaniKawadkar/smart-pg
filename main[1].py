94d497620fe0f6c2dec8686542fdf1f70f838e17
ShivaniKawadkar <shivanikawadkar89@gmail.com>
2026-08-23 20:30:06 +0530

    Fix backend main.py

--- main[1].py
+++ main[1].py
@@ -125,11 +125,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    
 
 
 def get_db():
